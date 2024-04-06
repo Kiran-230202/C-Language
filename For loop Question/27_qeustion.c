ffba03652ef73d7dd6011f6e63cb4212894482b1
Kiran Kune <101247743+Kiran-230202@users.noreply.github.com>
2024-04-06 11:53:19 +0530

    Update 27_qeustion.c

--- For loop Question/27_qeustion.c
+++ For loop Question/27_qeustion.c
@@ -1,30 +1,16 @@
 #include<stdio.h>
-void main(){
-    int i,n,sum=0;
-     printf("Enter rhe end point:");
-    scanf("%d",&n);
-    for(i=0;i<=n;i++){
-        if(i%2==1){
-            sum+=i/(i+1);
-        }
-        else{
-            sum-=i/(i+1);
-        }
-    }
-    printf("the sum is:%d",sum);
-}
-
 int main()
 {
-    double i, n,sum=0;
-    n=10;
-    for(i=1;i<=n;i++)
+    double i, n, sum = 0.0;
+    printf("Enter the end point: ");
+    scanf("%lf", &n);
+    for(i = 1; i <= n; i++)
     {
-        if ((int)i%2==1)
-            sum+=i/(i+1);
+        if ((int)i % 2 == 1)
+            sum += i / (i + 1);
         else
-            sum-=i/(i+1);
+            sum -= i / (i + 1);
     }
-    printf("Sum: %lf",sum);
+    printf("Sum: %lf", sum);
     return 0;
 }
